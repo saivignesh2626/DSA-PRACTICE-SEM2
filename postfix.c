a6dcbd8b3a72acffb959a78848a86ef0a0b0c8e3
saivignesh <saivigneshthanuku26@gmail.com>
2026-02-20 22:11:18 +0530

    updated postfix.c

--- postfix.c
+++ postfix.c
@@ -1,2 +1,73 @@
 #include<stdio.h>
-int main();
+#include<ctype.h>
+#include<string.h>
+#define N 50
+int stack[N];
+int top=-1;
+void push(int item);
+char pop();
+int main()
+{
+int len;
+char postfix[N];
+char a,b;
+
+printf("Enter posfix Expression : ");
+scanf("%s",postfix);
+
+len=strlen(postfix);
+
+    for(int i=0;i<len;i++)
+    {
+
+        if(isalnum(postfix[i]))
+        {
+            push(postfix[i]-'0');
+        }
+
+        else 
+        {
+
+            b=pop();
+            a=pop();
+
+            switch(postfix[i])
+            {
+                case '+': push(a+b);
+                            break;
+            
+            case '-': push(a-b);
+                            break;
+
+            case '*': push(a*b);
+                            break;
+
+            case '/': push(a/b);
+                            break;
+
+            case '%': push(a%b);
+                            break;
+            }
+
+  
+
+        }
+
+
+    }
+
+printf("Final Answer is : %d",pop());
+
+return 0;
+}
+
+void push(int item)
+{
+    stack[++top]=item;    
+
+}
+
+char pop()
+{
+    return stack[top--];
+}
